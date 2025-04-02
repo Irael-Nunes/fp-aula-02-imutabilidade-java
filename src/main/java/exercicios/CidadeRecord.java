e18bdde2f5592361e1fea86bfdd8c65c3c50f490
Nunes <ira.nunes21@gmail.com>
2025-04-02 20:48:04 -0300

    1) Record que representa uma cidade de forma imutável. Records são imutáveis por padrão - todos os campos são final e não podem ser alterados após a criação.

--- src/main/java/exercicios/CidadeRecord.java
+++ src/main/java/exercicios/CidadeRecord.java
@@ -1,4 +1,8 @@
 package exercicios;
 
+//1) Record que representa uma cidade de forma imutável.
+// Records são imutáveis por padrão - todos os campos são
+// final e não podem ser alterados após a criação.
+
 public record CidadeRecord(String nome) {
 }

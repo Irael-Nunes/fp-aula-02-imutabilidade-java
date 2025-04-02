fb02aa31c1bd721fa389604aa9ca9887b7f4a94c
Nunes <ira.nunes21@gmail.com>
2025-04-02 20:45:25 -0300

    2) Classe que representa uma cidade de forma mutável. Diferente do record, aqui podemos alterar os valores após a criação.

--- src/main/java/exercicios/Cidade.java
+++ src/main/java/exercicios/Cidade.java
@@ -1,5 +1,9 @@
 package exercicios;
 
+//2) Classe que representa uma cidade de forma mutável.
+// Diferente do record, aqui podemos alterar os valores
+// após a criação.
+
 public class Cidade {
     private String nome;
     public Cidade(String nome) {
